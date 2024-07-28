4ff82ab823d6e4834789fbe8ea4665a00f8c99c7
dhruvkaushik305 <dhruvkaushik305@gmail.com>
2024-07-28 17:11:47 +0530

    changed the overview layout to rectangle cards instead of a progress bar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,24 @@
-import ProgressBar from "../_components/ProgressBar";
-
 export default async function Page() {
   const budget = 1000;
   const spent = 600;
   const percentageSpent = (spent / budget) * 100;
   return (
-    <main className="flex flex-col rounded-l-[1rem] bg-slate-200 w-full">
-      <section className="p-5 text-gray-700 flex flex-col gap-4">
-        <header className="text-4xl">Welcome,</header>
-        <div className="flex gap-3 items-center">
-          <p className="text-nowrap text-xl">Budget consupmtion</p>
-          <ProgressBar percentageSpent={percentageSpent} />
+    <main className="flex flex-col gap-3 rounded-tl-[2rem] w-full p-5 bg-[#FAFAFA]  border border-slate-200">
+      <header className="text-3xl">Welcome,</header>
+      <section className="grid grid-cols-2 gap-5">
+        <div className="bg-secondary/80 text-white min-h-[200px] w-full rounded-3xl p-5 flex flex-col shadow-lg">
+          <p className="text-2xl font-medium">Budget Consumption</p>
+          <p className="text-right text-7xl grow flex items-center justify-end rounded-xl p-1">
+            {percentageSpent}%
+          </p>
         </div>
-        {/* <p>something</p> */}
+        <div className="bg-secondary/80 text-white min-h-[200px] w-full rounded-3xl p-5 flex flex-col shadow-lg">
+          <p className="text-2xl font-medium">Budget Left</p>
+          <p className="text-right text-7xl grow flex items-center justify-end rounded-xl p-1">
+            &#8377;{budget - spent}
+          </p>
+          <button className="self-end">View balance</button>
+        </div>
       </section>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#FFF",
+        secondary: "#0023FF",
+        tertiary: "#B388EB",
+      },
     },
   },
   plugins: [],
