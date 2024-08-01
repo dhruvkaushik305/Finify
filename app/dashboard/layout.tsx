48a4b38cf55e11cadeb6a8a1e172d1b36493075f
dhruvkaushik305 <dhruvkaushik305@gmail.com>
2024-08-01 15:36:50 +0530

    added nav buttons in the top bar and side bar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,35 @@
+import { IoCreateOutline } from "react-icons/io5";
+import { user } from "../lib/data";
+import Link from "next/link";
+import { GoHistory, GoHomeFill } from "react-icons/go";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex flex-col h-screen">
-      <nav className="min-h-[4rem]"></nav>
+      <nav className="min-h-[4rem] flex justify-around items-center bg-white">
+        <header className="text-3xl font-semibold">Finify</header>
+        <div className="flex gap-5 items-center">
+          <IoCreateOutline size={23} />
+          <div className="bg-gray-200 h-10 flex justify-center items-center w-10 rounded-full">
+            {user.name[0][0].toUpperCase()}
+          </div>
+        </div>
+      </nav>
       <section className="flex grow h-full">
-        <nav className="min-w-[18rem] h-full"></nav>
+        <nav className="min-w-[18rem] h-full flex flex-col items-center">
+          <Link href={"/dashboard"} className="w-full">
+            <button className="flex items-center gap-3 rounded-md p-3 w-full justify-center hover:shadow-md text-xl">
+              <GoHomeFill size={25} />
+              Dashboard
+            </button>
+          </Link>
+          <Link href={"/dashboard/history"} className="w-full">
+            <button className="flex items-center gap-3 rounded-md p-3 w-full justify-center hover:shadow-md text-xl">
+              <GoHistory size={25} />
+              Transactions
+            </button>
+          </Link>
+        </nav>
         {children}
       </section>
     </main>
