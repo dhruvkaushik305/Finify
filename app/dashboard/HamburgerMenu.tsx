058d6cb5993f1f928fbbe43dad313e084be8725f
dhruvkaushik305 <dhruvkaushik305@gmail.com>
2024-08-02 16:44:04 +0530

    added hamburger close button and shadow overlay. overlay still isn't perfect

--- app/dashboard/HamburgerMenu.tsx
+++ app/dashboard/HamburgerMenu.tsx
@@ -1,19 +1,33 @@
 "use client";
 import React from "react";
-import { IoMenuOutline } from "react-icons/io5";
+import { IoClose, IoMenuOutline } from "react-icons/io5";
 import NavItems from "./NavItems";
 export default function HamburgerMenu() {
   const [isOpen, setIsOpen] = React.useState(false);
   return (
     <>
-      <IoMenuOutline
-        size={30}
-        className="lg:hidden"
-        onClick={() => setIsOpen(!isOpen)}
-      />
+      {isOpen ? (
+        <IoClose
+          size={30}
+          className="lg:hidden"
+          onClick={() => setIsOpen(false)}
+        />
+      ) : (
+        <IoMenuOutline
+          size={30}
+          className="lg:hidden"
+          onClick={() => setIsOpen(true)}
+        />
+      )}
       {isOpen && (
-        <div className="absolute top-[4rem] bg-white w-full lg:hidden">
-          <NavItems />
+        <div className="absolute top-[4rem] left-0 bg-white w-full lg:hidden h-screen">
+          <div className="h-fit">
+            <NavItems />
+          </div>
+          <div
+            className="h-full bg-black/50 absolute w-full"
+            onClick={() => setIsOpen(false)}
+          ></div>
         </div>
       )}
     </>
