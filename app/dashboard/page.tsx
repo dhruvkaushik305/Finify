da4822ef6b0b043639f45f54498c31964cef2e49
dhruvkaushik305 <dhruvkaushik305@gmail.com>
2024-08-02 16:33:36 +0530

    made the dashbaord responpsive

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,27 +8,29 @@ export default async function Page() {
   const percentageSpent = Math.round((spent / budget) * 100);
 
   return (
-    <main className="flex flex-col justify-between rounded-tl-[2rem] w-full p-5 bg-[#FAFAFA]  border border-slate-200">
-      <header className="text-4xl font-medium">
+    <main className="flex flex-col justify-between gap-5 lg:gap-0 rounded-tl-[2rem] w-full p-3 lg:p-5 bg-[#FAFAFA]  border border-slate-200">
+      <header className="text-2xl lg:text-3xl xl:text-4xl font-medium">
         Welcome, {user.name.split(" ")[0]}
       </header>
-      <section className="grid grid-cols-2 gap-5">
-        <div className="bg-secondary/80 text-black min-h-[150px] w-full rounded-3xl p-5 flex flex-col shadow-lg">
-          <p className="text-2xl font-medium">Budget Consumption</p>
-          <p className="text-right text-6xl grow flex items-center justify-end rounded-xl p-1">
+      <section className="grid grid-cols-2 gap-3 lg:gap-5">
+        <div className="bg-secondary/80 text-black min-h-[150px] w-full rounded-3xl p-3 lg:p-5 flex flex-col shadow-lg">
+          <p className="text-xl md:text-2xl font-medium">Budget Consumption</p>
+          <p className="text-right text-4xl md:text-5xl xl:text-6xl grow flex items-center justify-end rounded-xl p-1">
             {percentageSpent}%
           </p>
         </div>
-        <div className="bg-secondary/80 text-black min-h-[150px] w-full rounded-3xl p-5 flex flex-col shadow-lg">
-          <p className="text-2xl font-medium">Budget Left</p>
-          <p className="text-right text-6xl grow flex items-center justify-end rounded-xl p-1">
+        <div className="bg-secondary/80 text-black min-h-[150px] w-full rounded-3xl p-3 lg:p-5 flex flex-col shadow-lg">
+          <p className="text-xl md:text-2xl font-medium">Budget Left</p>
+          <p className="text-right text-4xl md:text-5xl xl:text-6xl grow flex items-center justify-end rounded-xl p-1">
             &#8377;{budget - spent}
           </p>
           <BalanceButton />
         </div>
       </section>
       <section className="flex flex-col gap-3 items-start">
-        <header className="text-2xl font-medium">Weekly expediture</header>
+        <header className="text-xl md:text-2xl font-medium">
+          Weekly expediture
+        </header>
         <Graph />
       </section>
     </main>
